f6b4024614f5a73a0e2a03773fb3bab550956cf2
MikhailAkd <mihailbuspoi@gmail.com>
2022-08-11 11:41:28 +0300

    Выполнил домашнюю работу 9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        //Тут будет код :)
+        Author authorOne = new Author( "Александр ", "Сергеевич ", "Пушкин");
+        Author authorTwo = new Author( "Михаил ", "Юрьевич ", "Лермонтов");
+
+        Book bookOne = new Book ("Сказка о рыбаке и рыбке", authorOne, 1833);
+        Book bookTwo = new Book ("Герой нашего времени", authorTwo, 1840);
+
+        bookTwo.setYear(1843);
+
+        System.out.println("Список книг: ");
+        System.out.println("Наименование: " + bookOne.getName() + ". Автор: " + bookOne.getAuthor().authorNameFull() + ". Год издания: " + bookOne.getYear());
+        System.out.println("Наименование: " + bookTwo.getName() + ". Автор: " + bookTwo.getAuthor().authorNameFull() + ". Год издания: " + bookTwo.getYear());
     }
 }
